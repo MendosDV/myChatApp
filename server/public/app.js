3581ceb949801e93223795478af53adc786b5c07
Mickael Riss <mendos.prod@gmail.com>
2023-12-19 22:48:02 +0100

    Chat finish 🎉

--- server/public/app.js
+++ server/public/app.js
@@ -1,44 +1,119 @@
 // Initialise les websocket sur le port 3000
 const socket = io('ws://localhost:3500')
 
+const msgInput = document.querySelector('#message')
+const nameInput = document.querySelector('#name')
+const chatRoom = document.querySelector('#room')
 const activity = document.querySelector('.activity')
-const msgInput = document.querySelector('input')
+const usersList = document.querySelector('.user-list')
+const roomList = document.querySelector('.room-list')
+const chatDisplay = document.querySelector('.chat-display')
 
 // Envoi d'un message
 function sendMessage(e) {
-  // Retire le comportement de rafraichissement de la page par défaut
-  e.preventDefault()
+  e.preventDefault()   // Retire le comportement de rafraichissement de la page par défaut
 
   // Selectionne la balise input et envoie la valeur de l'input puis remet la chaine vide
-  if (msgInput.value) {
-    socket.emit('message', msgInput.value)
+  if (nameInput.value && msgInput.value && chatRoom.value) {
+    socket.emit('message', {
+        name: nameInput.value,
+        text: msgInput.value
+    })
     msgInput.value = ""
   }
   msgInput.focus()
 }
 
-// Ecoute notre formulaire et utilise la fonction d'envoi de message lors d'un event submit
-document.querySelector('form').addEventListener('submit', sendMessage)
+//
+function enterRoom(e) {
+  e.preventDefault()
+  if (nameInput.value && chatRoom.value) {
+    socket.emit('enterRoom', {
+        name: nameInput.value,
+        room: chatRoom.value
+    })
+  }
+}
+
+// Ecoute notre formulaire et utilise la fonction d'envoi de message lors d'un event
+document.querySelector('.form-chat')
+    .addEventListener('submit', sendMessage)
+
+document.querySelector('.form-join')
+    .addEventListener('submit', enterRoom)
+
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', nameInput.value)
+})
 
 // Ecoute les messages
 socket.on("message", (data) => {
   activity.textContent = ""
+  const { name, text, time } = data
   const li = document.createElement('li')
-  li.textContent = data
-  li.classList.add("bg-stone-100", "text-sm", "p-2.5", "mb-2",
-  "rounded-full", "rounded-tl-lg")
-  document.querySelector('ul').appendChild(li)
-})
+  li.className = 'post'
+  if (name === nameInput.value) li.className = 'post post--right'
+  if (name !== nameInput.value && name !== 'Admin') li.className = 'post post--left'
+  if (name !== 'Admin') {
+      li.innerHTML = `<div class="post__header ${name === nameInput.value
+          ? 'post__header--user'
+          : 'post__header--reply'
+          }">
+      <span class="post__header--name">${name}</span>
+      <span class="post__header--time">${time}</span>
+      </div>
+      <div class="post__text">${text}</div>`
+  } else {
+      li.innerHTML = `<div class="post__text italic">${text}</div>`
+  }
 
-msgInput.addEventListener('keypress', () => {
-  socket.emit('activity', socket.id.substring(0, 5))
+  // bg-stone-100 text-li
+
+  document.querySelector('.chat-display').appendChild(li)
+
+  chatDisplay.scrollTop = chatDisplay.scrollHeight
 })
 
 let activityTimer
 socket.on("activity", (name) => {
   activity.textContent = `${name} is typing...`
+  activity.style.opacity = 1
   clearTimeout(activityTimer)
   activityTimer = setTimeout(() => {
-    activity.textContent = ""
+    activity.style.opacity = 0
   }, 2000);
 })
+
+socket.on('userList', ({ users }) => {
+  showUsers(users)
+})
+
+socket.on('roomList', ({ rooms }) => {
+  showRooms(rooms)
+})
+
+function showUsers(users) {
+  usersList.textContent = ''
+  if (users) {
+      usersList.innerHTML = `<em>Users in ${chatRoom.value}:</em>`
+      users.forEach((user, i) => {
+          usersList.textContent += ` ${user.name}`
+          if (users.length > 1 && i !== users.length - 1) {
+              usersList.textContent += ","
+          }
+      })
+  }
+}
+
+function showRooms(rooms) {
+  roomList.textContent = ''
+  if (rooms) {
+      roomList.innerHTML = '<em>Active Rooms:</em>'
+      rooms.forEach((room, i) => {
+          roomList.textContent += ` ${room}`
+          if (rooms.length > 1 && i !== rooms.length - 1) {
+              roomList.textContent += ","
+          }
+      })
+  }
+}
